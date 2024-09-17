74ba306f54b08ae90e4093ce7a30d4ba05f25201
ShyamsaranRajendran <sarancode6@gamil.com>
2024-09-17 08:34:46 +0530

    Scaffold is build

--- lib/main.dart
+++ lib/main.dart
@@ -1,20 +1,51 @@
 import 'package:flutter/material.dart';
 
-void main(){
-  runApp(MyApp());
-}
+void main()=> runApp(new MaterialApp(
+  home: new MyApp(),
+));
 
-
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
   const MyApp({super.key});
 
   @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+
+
+class _MyAppState extends State<MyApp> {
+  @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      home: SafeArea(child: Text('Shyam')),
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Appbar demo'),
+        leading: IconButton(
+            onPressed: () {
+              print('Icon button click');
+            },
+            icon: Icon(Icons.menu)),
+            actions: <Widget>[
+              IconButton(onPressed: (){
+                print('search button is clicked');
+              }, icon: Icon(Icons.search)),
+              IconButton(
+              onPressed: () {
+                print('more button is clicked');
+              },
+              icon: Icon(Icons.more_vert))
+            ],
+      ),
     );
   }
 }
+
+
+
+
+
+
+
+
 
 // class MyApp extends StatefulWidget {
 //   const MyApp({super.key});
